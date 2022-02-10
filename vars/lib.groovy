307b46a85209b04bfa39dc4b4b779a0a4ef1bb29
Viktar Tsubulko <VIktar_Tsybulko@epam.com>
2022-02-10 19:56:46 +0300

    Fixed syntax

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,11 +1,11 @@
-def SimpleHelloFromLib () {
+def SimpleHelloFromLib {
     echo "Hallo from lib! Just for testing."
     script {
         bat "echo Hallo from lib! Just for testing."
     }
 }
 
-def HalloFromLib (name) {
+def HalloFromLib(name) {
     echo "Hallo from lib, ${name}!"
     script {
         bat "echo Hallo from lib, ${name}!"
